ccafa964adc1e893600f1b26a5218ea105a28f5d
swhyeon98 <smreo75@gmail.com>
2023-10-19 18:30:44 +0900

    Initial commit

---
+++ src/main/java/io/github/swhyeon98/board/Application.java
@@ -0,0 +1,13 @@
+package io.github.swhyeon98.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(io.github.swhyeon98.board.Application.class, args);
+    }
+
+}
